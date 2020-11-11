e29f931dd84899b19aee2f6a76e24792be3f9169
Pieter Fiers <pieter@pfiers.net>
2020-11-12 00:11:40 +0100

    Fix prerender origin

--- set-prerender-origin.sh
+++ set-prerender-origin.sh
@@ -2,3 +2,4 @@
 
 cd .presite
 find . -type f -name "*.html" -exec sed -E -i 's/http:\/\/(content.local|dist.local|localhost):8000/https:\/\/pfiers.net/g' {} +
+echo "Prerender origin set to production"
